fdae7974793dc3c96945740851da075c86dc0eb9
Rishisaiganesh <crazyganesh67@gmail.com>
2021-05-29 11:44:23 +0530

    [Rishi]Adding:Fabric in logical programs

---
+++ Logical_Program/Logical_Program/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical_Program
+{
+    class Fibonacci
+    {
+        public void Check()
+        {
+            int FirstNumber = 0, SecondNumber = 1, ThirdNumber, i, Number;
+            Console.WriteLine("Enter the Number of elements");
+            Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(FirstNumber + "" + "" + SecondNumber);
+            for(i=2; i<= Number; i++)
+            {
+                ThirdNumber = FirstNumber + SecondNumber;
+                Console.WriteLine(ThirdNumber + "");
+                FirstNumber = SecondNumber;
+                SecondNumber = ThirdNumber;
+
+            }
+        }
+    }
+}
---
+++ Logical_Program/Logical_Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Logical_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical programs");
+            Console.WriteLine("Press1: TO check Fibonacci");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+                    Fibonacci fib = new Fibonacci();
+                    fib.Check();
+                    break;
+
+
+            }
+        }
+    }
+}
